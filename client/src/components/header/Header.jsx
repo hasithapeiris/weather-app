acec9be73ddb376bf804fff0eca9997592cb7afe
Hasitha <hasithapeiris.me@gmail.com>
2024-10-05 21:11:04 +0530

    fix: update header links

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -6,19 +6,27 @@ const Header = () => {
     <div className={styles.header}>
       <div className={styles.headerWrapper}>
         <div className={styles.leftSide}>
-          <div
-            style={{
-              display: "flex",
-              alignItems: "center",
-              gap: 5,
-              cursor: "pointer",
-            }}
-          >
-            <img src="/logo.svg" alt="Weatherly" style={{ width: 30 }} />
-            <span style={{ fontSize: 24, fontWeight: 700, color: "#404040" }}>
-              Weatherly
-            </span>
-          </div>
+          <a href="/">
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                gap: 5,
+                cursor: "pointer",
+              }}
+            >
+              <img src="/logo.svg" alt="Weatherly" style={{ width: 30 }} />
+              <span
+                style={{
+                  fontSize: 24,
+                  fontWeight: 700,
+                  color: "#404040",
+                }}
+              >
+                Weatherly
+              </span>
+            </div>
+          </a>
         </div>
         <div className={styles.rightSide}>Logout</div>
       </div>
